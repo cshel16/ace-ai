a7fbd1c73b26967a2fb536a89911f77d32def6da
Caleb Shelton <crshelton16@gmail.com>
2026-06-23 21:52:06 -0400

    Back to basics

--- ace_ai/__main__.py
+++ ace_ai/__main__.py
@@ -11,7 +11,7 @@ def main() -> None:
     client = Client(tools)
     print("Welcome to Ace AI!")
     user_query = input("Ask me a question: ")
-    client.send_user_query(user_query)
+    print(client.send_user_query(user_query))
 
 if __name__ == "__main__":
     main()
